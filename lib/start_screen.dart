3811d6a8e6c1c7e393e1818567a69842c6488504
Giventus Marco <weishen735@gmail.com>
2025-09-05 22:47:35 +0700

    start screen template done

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,8 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text("Start Screen"),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            "assets/images/quiz-logo.png",
+            width: 300,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Learn Flutter the fun way!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            child: Text("Start Quiz"),
+          ),
+        ],
+      ),
     );
   }
 }
