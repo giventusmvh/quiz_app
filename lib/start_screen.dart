202f63e9e0d666d508d7f4160bfc8d660a5778a1
Giventus Marco <weishen735@gmail.com>
2025-09-07 01:34:02 +0700

    startscreen done

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -12,9 +14,10 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             "assets/images/quiz-logo.png",
             width: 300,
+            color: const Color.fromARGB(100, 255, 255, 255),
           ),
           SizedBox(
-            height: 30,
+            height: 70,
           ),
           Text(
             "Learn Flutter the fun way!",
@@ -26,12 +29,13 @@ class StartScreen extends StatelessWidget {
           SizedBox(
             height: 30,
           ),
-          OutlinedButton(
-            onPressed: () {},
+          OutlinedButton.icon(
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
-            child: Text("Start Quiz"),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text("Start Quiz"),
           ),
         ],
       ),
